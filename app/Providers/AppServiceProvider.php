a6da81bbacfd05e74b95e31146a71f68e5238a3e
Thomas Chong <thomaschong0929@gmail.com>
2023-02-13 12:54:43 +0800

    disabled auto migrate for sanctum

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        \Laravel\Sanctum\Sanctum::ignoreMigrations();
+
     }
 
     /**
